3ff34669131eebfcbd4c9324b24331c4f96d9cdd
elias098 <>
2023-04-18 23:36:24 +0200

    Converted project to use vite and svelte

---
+++ dist/evolve-automation.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       Evolve
+// @namespace  http://tampermonkey.net/
+// @version    3.3.1.108.9
+// @author     Fafnir, TMVictor, Vollch, schoeggu, elias098
+// @match      https://pmotschmann.github.io/Evolve/
+// @require    https://code.jquery.com/jquery-3.6.0.min.js
+// @require    https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// @grant      none
+// ==/UserScript==
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 // ==UserScript==
 // @name         Evolve
 // @namespace    http://tampermonkey.net/
